d4565ba5438fce9ac0a11ff93bf82a57f9e819f7
Hugo Chagas <75392085+h-chagas@users.noreply.github.com>
2023-02-21 17:05:51 +0000

    add content

---
+++ Module1/SQL/SQLite_/dbquery.py
@@ -0,0 +1,14 @@
+import sqlite3 as s
+
+con = s.connect('store.db')
+cur = con.cursor()
+
+#query = "SELECT * FROM USER"
+query = input("Please enter your query\n\n")
+
+data = cur.execute(query)
+
+for row in data:
+    print(row)
+con.commit()
+con.close()
