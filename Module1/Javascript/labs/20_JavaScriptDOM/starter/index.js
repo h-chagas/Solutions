426df273dd29e9f2e41f039d38680de10d5c2878
HugoCidralDasChagas <hugochagasuk@gmail.com>
2023-03-14 09:51:18 +0000

    add lab 20, resolved

--- Module1/Javascript/labs/20_JavaScriptDOM/starter/index.js
+++ Module1/Javascript/labs/20_JavaScriptDOM/starter/index.js
@@ -1,1 +1,18 @@
 const colours = ['red', 'green', 'blue', 'orange', 'lime','olive', 'black', 'yellow'];
+
+
+const buildP = function(placeholder, num) {
+    let i = 0;
+
+    do {
+        const p = document.createElement('p');
+        const text = document.createTextNode('Hi, my name is Hugo');
+        p.style.color = colours[parseInt(Math.random() * colours.length)];
+        p.appendChild(text);
+        placeholder.appendChild(p);
+        i++;
+    } while (i < num);
+
+}
+
+buildP(document.querySelector('#placeholder'), 5);
