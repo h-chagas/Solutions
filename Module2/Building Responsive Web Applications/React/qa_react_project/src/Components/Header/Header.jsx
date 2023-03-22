0d83933a5109d7d37cdd5917f513c3c4e59b23db
HugoCidralDasChagas <hugochagasuk@gmail.com>
2023-03-22 20:59:40 +0000

    create Header component and its css file

---
+++ Module2/Building Responsive Web Applications/React/qa_react_project/src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <h1 className={styles.h1}>Hugo's plant shop</h1>
+  )
+}
+
+export default Header
