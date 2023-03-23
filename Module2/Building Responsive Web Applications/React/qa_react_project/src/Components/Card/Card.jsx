13537ff1240a1dd3c88121c19346f8fe419bc689
HugoCidralDasChagas <hugochagasuk@gmail.com>
2023-03-23 16:18:40 +0000

    create toggle function to change style and text of the add to cart btn when clicked - useState to support rendering the changes

--- Module2/Building Responsive Web Applications/React/qa_react_project/src/Components/Card/Card.jsx
+++ Module2/Building Responsive Web Applications/React/qa_react_project/src/Components/Card/Card.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Card.module.css';
 
-const Card = ({imageUrl, name, price}) => {
+const Card = ({id, name, price, imageUrl}) => {
+  const [isActive, setIsActive] = useState(false);
+  
+  
+  function toggleClass() {
+    setIsActive(!isActive)
+  }
+
   return (
     
       <div className={styles.card}>
         <img className={styles.img} src={imageUrl} alt="plant" />
         <h2 className={styles.h2}>{name}</h2>
         <p className={styles.p}>£ {price.toFixed(2)}</p>
-        <input className={styles.btn} type="button" value="Add to cart" />
+        <input 
+        className={isActive ? styles.btnClicked : styles.btn} 
+        onClick={toggleClass} 
+        type="button" 
+        value={isActive ? "In Cart" : "Add to Cart"}
+        /> 
       </div>
     
   )
