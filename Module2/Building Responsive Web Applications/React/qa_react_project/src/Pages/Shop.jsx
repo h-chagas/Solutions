24fec6b618052bd73fc60f59413d691934d1e4a4
HugoCidralDasChagas <hugochagasuk@gmail.com>
2023-03-23 16:14:00 +0000

    receive data from api labelled as plantApi, then pass to CardsArea

--- Module2/Building Responsive Web Applications/React/qa_react_project/src/Pages/Shop.jsx
+++ Module2/Building Responsive Web Applications/React/qa_react_project/src/Pages/Shop.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import CardsArea from '../Components/CardsArea/CardsArea'
 import styles from './Shop.module.css'
 
-const Shop = () => {
+const Shop = ({plantApi}) => {
   return (
     <>
       <h2 className={styles.ourPlants}>Our plants</h2>
       <div className={styles.Shop}>
-        <CardsArea />
+        <CardsArea plantApi={plantApi} />
       </div>
     </>
   )
